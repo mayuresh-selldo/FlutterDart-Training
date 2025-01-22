53e9b1309285a775414df063edc3bafff90baaa9
mayuresh-selldo <mayuresh.chavan@sell.do>
2025-01-22 14:44:59 +0530

    Updated Collections File

--- Dart Files/Collections.dart
+++ Dart Files/Collections.dart
@@ -1,4 +1,6 @@
 void list() {
+  print("");
+  print("List Operations");
   List<int> arr = [1, 2, 3, 4];
   // arr.add("XYZ"); // this will give an error
 
@@ -16,19 +18,52 @@ void list() {
   print(arr);
 
   // And Many More {To Explore}
+
+  var list = [1, 2, 3];
+  assert(list.length == 3);
+  assert(list[1] == 2);
+
+  list[1] = 1;
+  assert(list[1] == 1);
+
+  // Create a Compile time constant list
+  var constantList = const [1, 2, 3];
+  print(constantList);
+
+  // constantList[0] = 10; // This Will give error (Unsupported operation: Cannot modify an unmodifiable list)
 }
 
 void set() {
+  // Set in Dart is Unordered as it does not follow indexing
+  // print(setprac[0]); // This is Throw Compile time error
+  print("");
+  print("Set Operations");
+
   Set<int> setprac = {1, 2, 3};
   print(setprac);
-
   print(setprac.elementAt(2));
 
-  // To Explore Ordered or unordered sets??
-  // To Explore Methods available
+
+  // add and addAll
+  var halogens = {'fluorine', 'chlorine', 'bromine', 'iodine', 'astatine'};
+  Set<String> element = {}; // This creates an empty set
+  element.add("Bromine"); // Adds One Element to the Set
+  print(element);
+  element.addAll(halogens); // Add the whole list of halogens into element set
+  print(element);
+
+
+  // Contains in Set or Not
+  if (element.contains('Fluorine')) {
+    print("Present in the Set");
+  } else {
+    print("Not Present");
+  }
 }
 
 void map() {
+  print("");
+  print("Map Operations");
   Map<String, dynamic> arr = {'1': 5, '2': 10, '3': 15};
 
   print(arr['2']);
@@ -36,16 +71,14 @@ void map() {
   arr['2'] = 30;
 
   print(arr['2']);
-
-  
 }
 
-void hashMap(){
+void hashMap() {
   // HashMap<String, int> str = {"1": 1}; // Check How to Initialize the values in HashMap
 }
 
-void main(){
+void main() {
   list();
   set();
-  map();  
+  map();
 }
