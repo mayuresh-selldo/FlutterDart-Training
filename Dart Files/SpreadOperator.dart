15e538e8db8a7116c6666b240562444a4a5363ed
mayuresh-selldo <mayuresh.chavan@sell.do>
2025-01-22 15:42:06 +0530

    Explored and Added Spread Operator File

---
+++ Dart Files/SpreadOperator.dart
@@ -0,0 +1,17 @@
+void main() {
+  /**
+   * Basically Spread Operator Inserts Multiple Values and 
+   * Appends it into the list we want  
+   */
+
+  var list1 = [10, 20, 30];
+  var list2 = [1, ...list1];
+  print(list2); // Output : [1,10,20,30]
+
+  //If the expression to the right of the spread operator might be null, you can
+  //avoid exceptions by using a null-aware spread operator (...?)
+
+  var nullList = [null];
+  var list3 = [0, ...?nullList];
+  print(list3);
+}
