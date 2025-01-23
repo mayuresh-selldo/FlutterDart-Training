89900d5a6adacdbf50dd7895453dafb3bd7b5e67
mayuresh-selldo <mayuresh.chavan@sell.do>
2025-01-23 10:34:11 +0530

    Explored and Added Typedef File

---
+++ Dart Files/typedefs.dart
@@ -0,0 +1,22 @@
+/**
+ * typedef—is a concise way to refer to a type. 
+ * Here's an example of declaring and using a type 
+ * alias named intList:
+ */
+
+typedef intList = List<int>;
+  typedef ListMapper<X> = Map<X, List<X>>;
+
+
+void typeDef() {
+  intList list = [1, 2, 3, 4];
+
+  print(list);
+
+  Map<String, List<String>> m1 = {}; // Verbose.
+  ListMapper<String> m2 = {}; // Same thing but shorter and clearer.
+}
+
+void main() {
+  typeDef();
+}
