79debc9b20b4ba17212765508e703331d409e747
mayuresh-selldo <mayuresh.chavan@sell.do>
2025-01-29 16:38:32 +0530

    Updated Shopping Game Added Wallet and Store Money Remaining

--- Dart Files/DartExersices/exercises/06.19-shopping-list.dart
+++ Dart Files/DartExersices/exercises/06.19-shopping-list.dart
@@ -1,37 +1,143 @@
-// # Exercise: Shopping List
+// // # Exercise: Shopping List
 
-// Given the following program:
+// // Given the following program:
 
-// ```dart
+// // ```dart
+// // void main() {
+// //   const bananas = 5;
+// //   const apples = 6;
+// //   const grains = {
+// //     'pasta': '500g',
+// //     'rice': '1kg',
+// //   };
+// //   const addGrains = true;
+// //   var shoppingList = {};
+// //   if (bananas > 0) {
+// //     shoppingList['bananas'] = bananas;
+// //   }
+// //   if (apples > 0) {
+// //     shoppingList['apples'] = apples;
+// //   }
+// //   if (addGrains) {
+// //     shoppingList.addAll(grains);
+// //   }
+// //   print(shoppingList);
+// //   // prints {bananas: 5, apples: 6, pasta: 500g, rice: 1kg}
+// // }
+// // ```
+
+// // Rewrite this code so that the `shoppingList` variable is initialized using **collection-if**, **collection-for** or **spreads**, and the program output is the same.
+
+// import 'dart:io';
+
 // void main() {
-//   const bananas = 5;
-//   const apples = 6;
-//   const grains = {
-//     'pasta': '500g',
-//     'rice': '1kg',
+//   var walmartStock = {
+//     'bananas': 10,
+//     'apples': 6,
+//     'grains': {
+//       'pasta': '500g',
+//       'rice': '102kg',
+//     }
 //   };
-//   const addGrains = true;
-//   var shoppingList = {};
-//   if (bananas > 0) {
-//     shoppingList['bananas'] = bananas;
-//   }
-//   if (apples > 0) {
-//     shoppingList['apples'] = apples;
-//   }
-//   if (addGrains) {
-//     shoppingList.addAll(grains);
-//   }
-//   print(shoppingList);
-//   // prints {bananas: 5, apples: 6, pasta: 500g, rice: 1kg}
-// }
-// ```
 
-// Rewrite this code so that the `shoppingList` variable is initialized using **collection-if**, **collection-for** or **spreads**, and the program output is the same.
+//   var ShoppingList = {};
+
+//   int convertToGrams(String quantity) {
+//     if (quantity[quantity.length - 2] == 'k') {
+//       return int.parse(quantity.replaceAll('kg', ''));
+//     } else if (quantity.endsWith('g')) {
+//       return int.parse(quantity.replaceAll('g', ''));
+//     }
+//     return 0;
+//   }
+
+//   String convertToString(int grams) {
+//     if (grams >= 1000) {
+//       return '${grams ~/ 1000}kg';
+//     } else {
+//       return '${grams}g';
+//     }
+//   }
+
+//   print("Current Stock : ${walmartStock}");
+//   walmartStock.forEach((key, value) {
+//     switch (key) {
+//       case 'bananas':
+//         if (value is int) {
+//           print("How Much Bananas do you want to buy? ");
+//           String? input = stdin.readLineSync();
+//           int banana = int.parse(input ?? '0');
+
+//           if (banana < 0) {
+//             throw "Invalid Number of ask";
+//           } else if (banana > value) {
+//             throw "Sorry We Don't Have that much bananas \n we have ${value} bananas";
+//           } else {
+//             ShoppingList['bananas'] = banana;
+//             walmartStock[key] = value - banana;
+//           }
+//         }
+//         break;
+
+//       case 'apples':
+//         if (value is int) {
+//           print("How Much Apples do you want to buy? ");
+//           String? input = stdin.readLineSync();
+//           int apple = int.parse(input ?? '0');
+//           try {
+//             if (apple < 0) {
+//               throw "Invalid Number of ask";
+//             } else if (apple > value) {
+//               throw "Sorry We Don't Have that much apples \n we have ${value} apples";
+//             } else {
+//               ShoppingList['apples'] = apple;
+//               walmartStock[key] = value - apple;
+//             }
+//           } catch (e) {
+//             print(e);
+//           }
+//         }
+//         break;
+
+//       case 'grains':
+//         var grainsStock = value as Map<String, String>;
+
+//         grainsStock.forEach((grain, quantity) {
+//           int stockQuantityInGrams = convertToGrams(quantity);
+
+//           print("How much $grain do you want to buy? (Available: $quantity)");
+
+//           String? input = stdin.readLineSync();
+//           int requestedQuantityInGrams = int.parse(input ?? '0');
+
+//           if (requestedQuantityInGrams < 0) {
+//             print("Invalid Quantity.");
+//           } else if (requestedQuantityInGrams > stockQuantityInGrams) {
+//             print(
+//                 "Sorry, we don't have that much $grain. Available: $quantity");
+//           } else {
+//             int updatedStockInGrams =
+//                 stockQuantityInGrams - requestedQuantityInGrams;
+//             grainsStock[grain] = convertToString(updatedStockInGrams);
+//             String value = requestedQuantityInGrams.toString() + 'g';
+//             ShoppingList[grain] = value;
+//           }
+//         });
+//         break;
+
+//       default:
+//         print("Bye!!!");
+//     }
+//   });
+
+//   print("Your Shopping List is : ${ShoppingList}");
+//   print("Walmart's Remaining Stock is : ${walmartStock}");
+// }
 
 import 'dart:io';
 
-void main() {
-  var walmartStock = {
+void shoppingListGame() {
+  var StockInShop = {
     'bananas': 10,
     'apples': 6,
     'grains': {
@@ -40,96 +146,140 @@ void main() {
     }
   };
 
-  var ShoppingList = {};
+  var pricesOfSingleGood = <String, int>{
+    'bananas': 5,
+    'apples': 30,
+    'pasta': 1,
+    'rice': 50,
+  };
 
-  int convertToGrams(String quantity) {
-    if (quantity[quantity.length - 2] == 'k') {
-      return int.parse(quantity.replaceAll('kg', ''));
+  var ShoppingCart = {};
+
+  int convertToInt(String quantity) {
+    if (quantity.endsWith('kg')) {
+      return int.parse(quantity.replaceAll('kg', '')) * 1000;
     } else if (quantity.endsWith('g')) {
       return int.parse(quantity.replaceAll('g', ''));
     }
     return 0;
   }
 
-  String convertToString(int grams) {
-    if (grams >= 1000) {
-      return '${grams ~/ 1000}kg';
-    } else {
-      return '${grams}g';
-    }
+  int calculatePrice(int quantity, int price) {
+    int totalPrice = quantity * price;
+    return totalPrice;
   }
 
-  print("Current Stock : ${walmartStock}");
-  walmartStock.forEach((key, value) {
-    switch (key) {
-      case 'bananas':
-        if (value is int) {
-          print("How Much Bananas do you want to buy? ");
-          String? input = stdin.readLineSync();
-          int banana = int.parse(input ?? '0');
-
-          if (banana < 0) {
-            throw "Invalid Number of ask";
-          } else if (banana > value) {
-            throw "Sorry We Don't Have that much bananas \n we have ${value} bananas";
-          } else {
-            ShoppingList['bananas'] = banana;
-            walmartStock[key] = value - banana;
-          }
+  int calculateStoreMoney() {
+    int money = 0;
+    StockInShop.forEach((key, value) {
+      if (value is int) {
+        if (pricesOfSingleGood.containsKey(key)) {
+          money += pricesOfSingleGood[key]! * value;
         }
-        break;
-
-      case 'apples':
-        if (value is int) {
-          print("How Much Apples do you want to buy? ");
-          String? input = stdin.readLineSync();
-          int apple = int.parse(input ?? '0');
-          try {
-            if (apple < 0) {
-              throw "Invalid Number of ask";
-            } else if (apple > value) {
-              throw "Sorry We Don't Have that much apples \n we have ${value} apples";
+      } else if (value is Map<String, String>) {
+        value.forEach((grainKey, quantity) {
+          if (pricesOfSingleGood.containsKey(grainKey)) {
+            int curr;
+            if (grainKey == "rice") {
+              curr = int.parse(quantity.replaceAll('kg', '')) *
+                  pricesOfSingleGood[grainKey]!;
             } else {
-              ShoppingList['apples'] = apple;
-              walmartStock[key] = value - apple;
+              curr = convertToInt(quantity) * pricesOfSingleGood[grainKey]!;
             }
-          } catch (e) {
-            print(e);
-          }
-        }
-        break;
-
-      case 'grains':
-        var grainsStock = value as Map<String, String>;
-
-        grainsStock.forEach((grain, quantity) {
-          int stockQuantityInGrams = convertToGrams(quantity);
-
-          print("How much $grain do you want to buy? (Available: $quantity)");
-
-          String? input = stdin.readLineSync();
-          int requestedQuantityInGrams = int.parse(input ?? '0');
-
-          if (requestedQuantityInGrams < 0) {
-            print("Invalid Quantity.");
-          } else if (requestedQuantityInGrams > stockQuantityInGrams) {
-            print(
-                "Sorry, we don't have that much $grain. Available: $quantity");
-          } else {
-            int updatedStockInGrams =
-                stockQuantityInGrams - requestedQuantityInGrams;
-            grainsStock[grain] = convertToString(updatedStockInGrams);
-            String value = requestedQuantityInGrams.toString() + 'g';
-            ShoppingList[grain] = value;
+            money += curr;
           }
         });
-        break;
+      }
+    });
+    return money;
+  }
 
-      default:
-        print("Bye!!!");
-    }
-  });
+  bool gameOver = false;
+  int myWalletMoney = 100000;
+  int cartPrice = 0;
+  int StoreMoneyRemainingToSell = calculateStoreMoney();
+  print(StoreMoneyRemainingToSell);
 
-  print("Your Shopping List is : ${ShoppingList}");
-  print("Walmart's Remaining Stock is : ${walmartStock}");
+  void playGame() {
+    StockInShop.forEach((key, availableQuantity) {
+      print("Current Stock we have in shop is : ${StockInShop}");
+      if (key == "bananas") {
+        print(
+            "How Much Bananas Do you want to buy? We have $availableQuantity available");
+
+        int quantity = int.parse(stdin.readLineSync()!);
+        int price = pricesOfSingleGood[key]!;
+        int totalPrice = calculatePrice(quantity, price);
+
+        if (totalPrice > myWalletMoney) {
+          print("You Dont Have Enough Money Darling");
+          return;
+        }
+
+        if (availableQuantity is int) {
+          if (quantity > availableQuantity) {
+            print("We Dont have that much $key, Sorry");
+            return;
+          } else if (quantity <= availableQuantity) {
+            StockInShop[key] = availableQuantity - quantity;
+            ShoppingCart[key] = quantity;
+            print("Added $quantity $key in shopping Cart Successfully");
+            myWalletMoney -= totalPrice;
+            cartPrice += totalPrice;
+          }
+        } else {
+          print("Enter Valid Integer Value");
+          return;
+        }
+      }
+
+      if (key == "apples") {
+        print(
+            "How Much $key Do you want to buy? we have $availableQuantity available");
+
+        int quantity = int.parse(stdin.readLineSync()!);
+        int price = pricesOfSingleGood[key]!;
+        int totalPrice = calculatePrice(quantity, price);
+
+        if (totalPrice > myWalletMoney) {
+          print("You Dont Have Enough Money Darling");
+          return;
+        }
+
+        if (availableQuantity is int) {
+          if (quantity > availableQuantity) {
+            print("We Dont have that much $key, Sorry");
+            return;
+          } else if (quantity <= availableQuantity) {
+            StockInShop[key] = availableQuantity - quantity;
+            ShoppingCart[key] = quantity;
+            print("Added $quantity $key in shopping Cart Successfully");
+            myWalletMoney -= totalPrice;
+            cartPrice += totalPrice;
+          }
+        } else {
+          print("Enter Valid Integer Value");
+          return;
+        }
+      }
+    });
+
+    StoreMoneyRemainingToSell = calculateStoreMoney();
+
+    print("");
+    print("Current Stock we have in shop is : ${StockInShop}");
+    print("Remaining Money in your Wallet is : $myWalletMoney");
+    print("Shopping cart is ${ShoppingCart} and CartTotal is $cartPrice");
+    print("Store Money to sell remaining $StoreMoneyRemainingToSell");
+
+    gameOver = true;
+  }
+
+  do {
+    playGame();
+  } while (!gameOver);
+}
+
+void main() {
+  shoppingListGame();
 }
