a5fc934fd5645c242d18a0e25beef637cadae6ea
mayuresh-selldo <mayuresh.chavan@sell.do>
2025-02-03 11:34:23 +0530

    Updating Asynchronous Programming File

--- Dart Files/AsynchronousProgramming.dart
+++ Dart Files/AsynchronousProgramming.dart
@@ -1,14 +1,17 @@
-Future<void> AsyncAwaitFutures() async {
-  Future<String> returnString() async {
-    return Future.delayed(
-        const Duration(seconds: 10), () => "Returning the Strings");
-  }
-
-  var response = await returnString();
-  print(response);
-  print("Hello World");
+Future<String> createOrderMessage() async {
+  var order = await fetchUserOrder();
+  return 'Your order is: $order';
 }
 
-void main() {
-  AsyncAwaitFutures();
+Future<String> fetchUserOrder() =>
+    // Imagine that this function is
+    // more complex and slow.
+    Future.delayed(
+      const Duration(seconds: 2),
+      () => 'Large Latte',
+    );
+
+Future<void> main() async {
+  print('Fetching user order...');
+  print(await createOrderMessage());
 }
